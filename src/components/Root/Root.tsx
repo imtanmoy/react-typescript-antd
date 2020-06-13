aa2f50da96acec00faf2e6b631b4f7a266f117c1
Tanmoy <Tanmoy Banik>
2020-06-13 10:40:39 +0600

    react helmet added for metadata update

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { History } from 'history';
 import { ConnectedRouter } from 'connected-react-router';
+import { HelmetProvider } from 'react-helmet-async';
 import { App } from '../App/App';
 import { Store } from '../../store';
 
@@ -14,7 +15,9 @@ export const Root: React.FC<RootProps> = ({ history, store }) => {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <App history={history} />
+        <HelmetProvider>
+          <App history={history} />
+        </HelmetProvider>
       </ConnectedRouter>
     </Provider>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import BasicLayout from '../../layouts/BasicLayout';
 
 export const Home = () => {
   return (
-    <BasicLayout>
-      <div style={{ height: '2000px' }}>
-        <h1>Home Page</h1>
-      </div>
-    </BasicLayout>
+    <>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      <BasicLayout>
+        <div style={{ height: '2000px' }}>
+          <h1>Home Page</h1>
+        </div>
+      </BasicLayout>
+    </>
   );
 };
